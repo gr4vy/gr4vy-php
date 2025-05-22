52049613352f0ea6eab0b6e94f94c092f099b71e
Cristiano Betta <cbetta@gr4vy.com>
2025-05-22 15:13:37 +0100

    chore: Add sodium polyfill

--- src/SDKConfiguration.php
+++ src/SDKConfiguration.php
@@ -33,11 +33,11 @@ class SDKConfiguration
 
     public string $openapiDocVersion = '1.0.0';
 
-    public string $sdkVersion = '1.0.0-beta.1';
+    public string $sdkVersion = '1.0.0-beta.2';
 
     public string $genVersion = '2.607.0';
 
-    public string $userAgent = 'speakeasy-sdk/php 1.0.0-beta.1 2.607.0 1.0.0 gr4vy/gr4vy-php';
+    public string $userAgent = 'speakeasy-sdk/php 1.0.0-beta.2 2.607.0 1.0.0 gr4vy/gr4vy-php';
     /** @var array<string, array<string, array<string, mixed>>> */
     public ?array $globals = [
         'parameters' => [],
